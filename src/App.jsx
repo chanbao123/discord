3bdc83087fcccc861abe27c11fbafcc01780e658
chanbao <chabao172002@gmail.com>
2023-07-26 23:09:22 +0700

    new project

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./home";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  const [islogin, setLogin] = useState(false);
+  const [user, setUser] = useState();
+  const id_user = localStorage.getItem("id");
+  const discordauth = localStorage.getItem("discordauth");
+  const handlerLogin = async () => {
+    const id = uuidv4();
+    const reponse = await axios.get(
+      `http://144.126.145.81:8888/auth/discord/${id}`
+    );
+    window.location.href = reponse.data.auth;
+    localStorage.setItem("user", reponse.data.user);
+    localStorage.setItem("id", id);
+    console.log(reponse.data);
+  };
+  useEffect(() => {
+    if (id_user !== null) {
+      try {
+        const getData = async () => {
+          const data = await axios.get(
+            `http://144.126.145.81:8888/api/datauser/${id_user}`
+          );
+          setUser(data.data);
+          localStorage.removeItem("id");
+          localStorage.removeItem("user");
+          localStorage.setItem("discordauth", data.data.access_token);
+          setLogin(true);
+          console.log(data.data);
+        };
+        getData();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [id_user]);
+
+  useEffect(() => {
+    if (discordauth !== null) {
+      try {
+        const getData = async () => {
+          const data = await axios.get(
+            `http://144.126.145.81:8888/api/datauser/${discordauth}`
+          );
+          setUser(data.data);
+          setLogin(true);
+          console.log(data.data);
+        };
+        getData();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [discordauth]);
+
+  return (
+    <>
+      {islogin ? (
+        <Home userdata={user} />
+      ) : (
+        <div className="flex justify-center items-center w-full h-screen">
+          <button
+            type="submit"
+            className="w-20 h-10 border rounded-lg focus:outline-none hover:border-cyan-500 duration-300"
+            onClick={handlerLogin}
+          >
+            LOGIN
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/home.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+
+function Home({ userdata }) {
+  return (
+    <div>
+      <div className="flex  pt-6 w-100%">
+        <div className="bg-blue-700 w-64 h-80 text-gray-300 flex-col border-1 rounded-lg">
+          <h3 className="px-20 pt-6">MENU</h3>
+          <div>
+            <ul className="pl-5 pt-6 space-y-4">
+              <li>
+                <a href="" className="hover:text-white ">
+                  Option 1
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:text-white">
+                  Option 2
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:text-white">
+                  Option 3
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="bg-blue-700 px-8 h-20 flex-col justify-center pt-5  w-full items-center mx-8 rounded-lg border-1  ">
+          <div className="flex items-center gap-20 ">
+            <div>
+              <h2 className="font-semibold text-2xl">TECHNOLOGY PRO</h2>
+            </div>
+            <div className="flex mx-8 items-center justify-center pl-40">
+              <input
+                type="text"
+                placeholder="Search"
+                className="rounded-lg border-1"
+              />
+              <button></button>
+            </div>
+            <div>
+              <p>{userdata.user.global_name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="justify-center items-center grid grid-cols-5 gap-5 ">
+        {userdata.guilds.map((v) => {
+          return (
+            <div
+              className="flex flex-col border border-indigo-600 rounded-md bg-slate-600 p-8 justify-center items-center"
+              key={v.id}
+            >
+              <div>
+                <img
+                  src={`https://cdn.discordapp.com/icons/${v.id}/${v.icon}.png`}
+                  alt={`${v.icon}`}
+                />
+              </div>
+              <div>
+                <a href="">
+                  <p>{v.name}</p>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="justify-center items-center flex w-full h-96 gap-20 ">
+        <div className="flex w-250 h-400 flex-col border border-indigo-600 rounded-md bg-slate-600 px-8">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <a href="">
+              <p>AAA</p>
+            </a>
+            <a href="">
+              <p>2000$</p>
+            </a>
+          </div>
+          <div className="gap-5 flex">
+            <div>
+              <button>BUY</button>
+            </div>
+            <div>
+              <button>ADD</button>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-250 h-400 flex-col border border-indigo-600 rounded-md bg-slate-600 px-8">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <a href="">
+              <p>AAA</p>
+            </a>
+            <a href="">
+              <p>2000$</p>
+            </a>
+          </div>
+          <div className="gap-5 flex">
+            <div>
+              <button>BUY</button>
+            </div>
+            <div>
+              <button>ADD</button>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-250 h-400 flex-col border border-indigo-600 rounded-md bg-slate-600 px-8">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <a href="">
+              <p>AAA</p>
+            </a>
+            <a href="">
+              <p>2000$</p>
+            </a>
+          </div>
+          <div className="gap-5 flex">
+            <div>
+              <button>BUY</button>
+            </div>
+            <div>
+              <button>ADD</button>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-250 h-400 flex-col border border-indigo-600 rounded-md bg-slate-600 px-8">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <a href="">
+              <p>AAA</p>
+            </a>
+            <a href="">
+              <p>2000$</p>
+            </a>
+          </div>
+          <div className="gap-5 flex">
+            <div>
+              <button>BUY</button>
+            </div>
+            <div>
+              <button>ADD</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row pt-6 w-100% h-auto gap-16">
+        <div className=" w-50% h-40 float-left block ml-4">
+          <h3 className="font-semibold text-2xl">TECHNOLOGY PRO 16</h3>
+          <p className="block">
+            Data transmission has three aspects: transmission, propagation,
+            <br />
+            and reception.It can be broadly categorized as broadcasting,
+            <br /> in which information is transmitted unidirectionally
+            downstream,
+            <br /> or telecommunications, with bidirectional upstream and
+            downstream channels. <br />
+            XML has been increasingly employed as a means of data interchange
+            since the early 2000s,
+            <br />
+            particularly for machine-oriented interactions such as those
+            involved in web-oriented protocols such as SOAP,
+            <br />
+            describing "data-in-transit rather than... data-at-rest".
+          </p>
+        </div>
+        <div className=" w-50% float-right ml-4 justify-center items-center h-40 pl-40">
+          <h3>dsfsdf</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
